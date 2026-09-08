a94398fce273f6580bf6f694f239f29e57bad88c
Daniel Kapadia <business@ddkesports.com>
2026-09-07 21:20:39 -0500

    test(replay): exercise NPC scene rotation through send tables
    Signed-off-by: Daniel Kapadia <business@ddkesports.com>

--- npc-facing_test.go
+++ npc-facing_test.go
@@ -5,10 +5,11 @@ import "testing"
 // TestNpcFacingPreservesSceneRotation checks the actual network field used by NPCs.
 func TestNpcFacingPreservesSceneRotation(t *testing.T) {
 	// Provide the recorded skeleton rotation without a player eye-angle field.
-	entity := newEntity(7, 2, &entityClass{name: "CNPC_Boss_Tier2"})
+	class := &entityClass{name: "CNPC_Boss_Tier2", serializer: &serializer{fields: []*field{
+		{sendNode: "CBodyComponent.m_skeletonInstance", varName: "m_angRotation"},
+	}}}
+	entity := newEntity(7, 2, class)
 	path := fieldPath{last: 0}
-	const name = "CBodyComponent.m_skeletonInstance.m_angRotation"
-	entity.paths[name] = path
 	entity.state.set(path, []float32{0, 60, 0})
 	entity.fieldTicks[path] = 100
 	parser := &Parser{clock: newClock(), entities: map[int32]*Entity{7: entity}}
